44345e194c615cbdffe4110af3070c028d667f91
BennyZHill <ben.h@builtbypixel.com>
2021-07-19 14:56:33 +0100

    trumps

---
+++ cards.js
@@ -0,0 +1,47 @@
+const trumpCards = [
+{ id: 0, 
+  name: 'Luffy',
+  attack: 70,
+  defense: 80,
+  speed: 84,
+  intelligence: 20,
+  bounty: 1500000000
+},
+{ id: 1,
+name: 'Zoro',
+attack: 88,
+defense: 66,
+speed:77,
+intelligence: 15,
+bounty: 320000000
+
+},
+{ id: 2,
+    name: 'Sanji',
+    attack: 77,
+    defense: 50,
+    speed:88,
+    intelligence: 25,
+    bounty: 330000000,
+    
+    
+    },
+    { id: 3,
+        name: 'Nami',
+        attack: 50,
+        defense: 40,
+        speed:40,
+        intelligence: 88,
+        bounty: 66000000
+ },
+ { id: 4,
+    name: 'Jimbei',
+    attack: 82,
+    defense: 78,
+    speed:60,
+    intelligence: 60,
+    bounty: 438000000
+},
+]
+
+console.log(trumpCards)
